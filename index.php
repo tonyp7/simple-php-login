2d66e2509e81a76e88cdd687c0826e49974aa4fc
tonyp7 <bubbathemaster@gmail.com>
2022-09-06 17:49:54 +0800

    simple login

--- index.php
+++ index.php
@@ -2,6 +2,23 @@
 session_start();
 require_once('config.inc.php');
 
+//process logins
+if(isset($_POST['username']) && isset($_POST['password'])){
+
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    foreach ($login['native'] as $v){
+        if ( $v['user'] == $username && $v['password'] == $password ){
+            $_SESSION['active'] = true;
+            $_SESSION['method'] = 'native';
+            $_SESSION['user'] = $username;
+        }
+    }
+}
+
+
+
 if($_SESSION['active'] == false){
 	include('login.html');
     exit;
