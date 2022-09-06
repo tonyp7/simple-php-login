02515bd4c15c287e421434943b9ddeac9bb42315
tonyp7 <bubbathemaster@gmail.com>
2022-09-06 17:27:37 +0800

    Update sample.config.inc.php

--- sample.config.inc.php
+++ sample.config.inc.php
@@ -4,7 +4,7 @@
  * This host the configuration file for php-simple-login; most notably authorized users
  */
 
-$login['native'] array( array( 'user'=>'user1', 'password'=>'password1'), array( 'user'=>'user2', 'password'=>'password2') );
+$login['native'] = array( array( 'user'=>'user1', 'password'=>'password1'), array( 'user'=>'user2', 'password'=>'password2') );
 
 $login['google'] = array( 'foo@gmail.com', 'bar@gmail.com' );
 $login['github'] = array( 'tonyp7' );
