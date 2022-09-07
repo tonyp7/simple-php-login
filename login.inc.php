44f28e9e6430aa21e0b2ca79ec38bff867acb610
tonyp7 <bubbathemaster@gmail.com>
2022-09-07 13:37:18 +0800

    Create login.inc.php

---
+++ login.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+define('GITHUB_OAUTH_URL', 'https://github.com/login/oauth/authorize')
+
+function print_github_button($client_id, $return_uri){
+
+    printf('
+<a href="%s?client_id=%s%s&allow_signup=false" id="github-button" class="w-100 btn btn-lg btn-block btn-social btn-github mt-1">
+    <i class="bi bi-github"></i> Sign in with GitHub
+</a>', GITHUB_OAUTH_URL, 
+        $client_id,
+        empty($return_uri)?'':sprintf('&return_uri=%s',$return_uri)
+    );
+
+
+}
+
+
+?>
